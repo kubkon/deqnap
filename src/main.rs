b83ec3e4ab1b5f45a6684354f83dbf818a2ca371
Jakub Konka <kubkon@jakubkonka.com>
2015-11-14 13:47:33 +0000

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+DEQNAPify your Dropbox.
+
+Usage:
+    deqnap <path>
+    deqnap (-h | --help)
+    deqnap --version
+
+Options:
+    -h --help   Prints this message.
+    --version   Prints version.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_path: String,
+}
 
 fn walk_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
     if try!(fs::metadata(dir)).is_dir() {
@@ -21,8 +43,12 @@ fn do_something(entry: &DirEntry) {
 }
 
 fn main() {
-    let p = Path::new(".");
-    match walk_dirs(p, &do_something) {
+    // Parse arguments
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    let path = Path::new(&args.arg_path);
+    match walk_dirs(path, &do_something) {
         Err(err) => panic!("Unexpected error occurred: {}", err),
         Ok(()) => ()
     }
