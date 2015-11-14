1b3f13ee8c853ffb273fffcf7ab857f92edf43ce
Jakub Konka <kubkon@jakubkonka.com>
2015-11-14 11:12:38 +0000

    Walk all directories recursively

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@ use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
 
+fn walk_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
+    if try!(fs::metadata(dir)).is_dir() {
+        for entry in try!(fs::read_dir(dir)) {
+            let entry = try!(entry);
+            if try!(fs::metadata(entry.path())).is_dir() {
+                try!(walk_dirs(&entry.path(), cb));
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn do_something(entry: &DirEntry) {
+    println!("{}", entry.file_name().to_str().unwrap());
+}
 
 fn main() {
     let p = Path::new(".");
-    let entries = match fs::read_dir(p) {
-        Err(err) => panic!(err),
-        Ok(e) => e
-    };
-    for entry in entries {
-        let entry = entry.unwrap();
-        let entry_path = entry.path();
-        let meta = match fs::metadata(entry_path) {
-            Err(err) => panic!(err),
-            Ok(meta) => meta
-        };
-        if meta.is_file() {
-            println!("{}", entry.file_name().to_str().unwrap());
-        }
-    }
+    let _ = walk_dirs(p, &do_something);
 }
